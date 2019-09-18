e9fc3fa38c11c7a3cde26e28cddbb6c3e4187369
notmaiku <notmaiku@protonmail.com>
2019-09-17 22:37:55 -0400

    trying a component for not found

--- src/router.js
+++ src/router.js
@@ -1,6 +1,7 @@
 import Vue from "vue";
 import Router from "vue-router";
 import Home from "./views/Home.vue";
+import NotFound from './components/NotFound.vue'
 
 Vue.use(Router);
 export default new Router({
