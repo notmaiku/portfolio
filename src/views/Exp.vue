8fd4eeeba6efd76499242751a7a7054b1bed07f3
notmaiku <30605989+notmaiku@users.noreply.github.com>
2019-09-26 15:24:03 -0400

    redid the exprience page

--- src/views/Exp.vue
+++ src/views/Exp.vue
@@ -1,52 +1,64 @@
 <template>
-<body>
+<body id="exp">
   <Header />
   <Navigation />
   <main class="site-wrap">
-    <h4>Resume</h4>
-    <h1>Full-Stack Software Developer</h1>
-    <h2>Technical</h2>
-    <p>
-      NodeJS, MongoDB, HTML/CSS, VueJS, VPS: Digital Ocean/AWS, SSH, MySQL, OSX, Swift,
-      Windows, C#, JavaScript, Unity, WordPress, Adobe Photoshop, TypeScript, Angular 2+, Git, Vim
-    </p>
+    <h4>Basically A Resume</h4>
+    <h1>Full-Stack Stuff</h1>
+    <h2>Work Experience</h2>
+    <h3>Crier Media Group(Front-end): March 2019 - July 2019</h3>
+    <ul>
+      <li>For WordPress what I did was Design -> <strong>custom template HTML/CSS</strong> -> plug into WordPress.</li>
+      <li>
+        Working on the main site which is in Laravel/PHP. I styled the dashboard and <strong>proposed
+        designs</strong>.
+      </li>
+      <li>
+        Then worked out a readability script <strong>crawler</strong> (article without ads), and debug front end
+        bugs.
+      </li>
+    </ul>
+    <h3>Primary Web Services(Front-end): July 2018 - March 2019</h3>
+    <ul>
+      <li>
+        At this company I was working on a high-profile wireless infrastructure
+        application including <strong>live data visualization</strong> and <strong>micro-service ingestion</strong>.
+      </li>
+      <li>I implemented the dynamic charting, data display and <strong>routing modules</strong>, and built the application to be a multi-modular application in <strong>Angular 6/7</strong>.</li>
+      <li>Agile workflow with client and built in a test-driven environment.</li>
+    </ul>
     <h2>Side Projects</h2>
     <h3>Coding for fun (Backend): Ongoing</h3>
-    <p>
-      Developed a discord bot that manages a many to many relationships between users and
-      servers . It’s written in NodeJS and uses Mongo/NoSQL for the database and is relational with
-      level 1 embedding.
-    </p>
-    <p>
-      The project is structured to be run based reading files, NodeJS “fs”, in a module.exports fashion.
-      With each command having its own file in the respected, event, command folder.
-    </p>
-    <p>
-      The bot is able to moderate and manager servers and play text trivia and gacha games per
-      server. Query and display stats for the server or user, which is also optimized, based off timed
-      events.
-    </p>
-    <h2>Work Experience</h2>
-    <h3>Crier Media Group(Front-end): March - July</h3>
-    <p>For WordPress what I did was Design -> code out HTML/CSS -> plug into WordPress.</p>
-    <p>
-      Working on the main site which is in Laravel/PHP. I styled the dashboard and proposed
-      designs.
-    </p>
-    <p>
-      Then worked out a readability script (article without ads), and debug front end
-      bugs.
-    </p>
-    <h3>Intern Freelancing: July 2018 - Present</h3>
-    <p>
-      I’ve interned with Primary Web Services, LLC working on a high-profile wireless infrastructure
-      application including live data visualization and micro-service ingestion.
-    </p>
-    <p>I implemented the dynamic charting, data display and routing modules, and built the application to be a multi modular application in Angular 6/7.</p>
-    <p>Agile workflow with client and built in a test-driven environment.</p>
+    <ul>
+      <li>
+        Developed a discord bot that manages a many to many relationships between users and
+        servers. It’s written in NodeJS and uses <strong>Mongo/NoSQL</strong> for the database and is relational with
+        level 1 embedding.
+      </li>
+      <li>
+        The project is structured to be run based reading files, NodeJS “fs”, in a <strong>module.exports</strong> fashion.
+        With each command having its own file in the respected, event, command folder.
+      </li>
+      <li>
+        The bot is able to moderate and manager servers and play text trivia and gacha games per
+        server. <strong>Map reduce</strong> and utilize <strong>Aggergation Pipeline</strong> for the results of the server or user.
+      </li>
+    </ul>
+    <h3>Portfolio: Ongoing</h3>
+    <ul>
+      <li>
+        I created <strong>Single Page Application</strong> written in <strong>VueJS</strong> , so it has Vue
+        files written to be multi component
+      </li>
+      <li>It’s hosted in AWS, and it’s deployed in <strong> Docker</strong> with <strong>NGINX</strong>, which makes it redeployable anywhere</li>
+      <li>For unknown routes there is a <strong>fallback</strong> error page, and for any existing route you can travel directly to by /route-name</li>
+      <li>Nav that follows was built with pure CSS , and the carousel is a component</li>
+    </ul>
     <h2>Education</h2>
     <h3>Northern Virginia Community College: August 2016 – May 2019</h3>
-    <p>Information Technology Associate of Science Degree</p>
+    <ul>
+    <li>Information Technology Associate of Science Degree</li>
+    </ul>
   </main>
 </body>
 </template>
@@ -68,18 +80,4 @@ export default {
 </script>
 
 <style>
-h1 {
-  font-size: 34px;
-  color: rgb(101, 189, 177);
-  text-shadow: none;
-}
-h3 {
-  font-size: 24px;
-  border-bottom: solid rgb(101, 189, 177) 3px;
-  padding-bottom: 10px;
-}
-h4 {
-  font-size: 42px;
-  text-decoration: underline;
-}
 </style>
